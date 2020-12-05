4abbc8ebeb0bc92bb32dddee3720978612c37c8e
Lisa-Tera <eun01632@naver.com>
2020-12-05 23:22:07 +0900

    v2

--- go_gorutine_02.go
+++ go_gorutine_02.go
@@ -5,8 +5,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
-
+func responseSize(url string){
+	fmt.Println(url)
+	//resp에는 어떤 사이트든 그 사이트에 대한 정보를 넣고
+	resp, e:= http.Get(url) //가변처리가 되도록 넘겨받은 url로 넣고
+	if e != nil{
+		log.Fatal(e)
+	}
+	defer resp.Body.Close()//resp가 끝나는 시점에 연결을 끝내주고
+	body ,e := ioutil.ReadAll(resp.Body)
+	if e !=nil{
+		log.Fatal(e)
+	}
+	fmt.Println(len(body))
+}
+func main()  {
+	//순서가 바뀌지 않음
+	responseSize("https://www.inhatc.ac.kr")
+	responseSize("http://google.com")
+	responseSize("http://harvard.edu")
+	responseSize("https://www.naver.com")
+	time.Sleep(time.Second*5)
+}
+/*
 func main(){
 	resp, e := http.Get("https://www.inhatc.ac.kr")
 	if e != nil{
@@ -21,3 +44,4 @@ func main(){
 	//fmt.Println(string(body)) string을 쓰면 html같은 소스 코드가 나옴
 	fmt.Println(len(body))
 }
+*/
